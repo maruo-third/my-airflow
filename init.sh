f6aa339335da49e3b418faaa5c23e0b4f529ad9c
maruo-third <hr.maruoka@gmail.com>
2024-12-06 16:05:51 +0900

    add docker-compose kit

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+source .env
+
+mkdir -p \
+    $AIRFLOW_PROJ_DIR/dags\
+    $AIRFLOW_PROJ_DIR/logs\
+    $AIRFLOW_PROJ_DIR/plugins\
+    $AIRFLOW_PROJ_DIR/config
+
+if [ -z $AIRFLOW_UID ]; then
+  echo "AIRFLOW_UID=$(id -u)" >> .env
+fi
+
+#docker compose up airflow-init
+docker compose up -d
